2665dd7b0460837de2110f1d4bb4948567f1b3e0
mcodemax <mwalterjohnson7@gmail.com>
2022-06-04 22:28:21 -0700

    add form editing to UpdateUser

--- src/UpdateUser.js
+++ src/UpdateUser.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 function UpdateUser() {
   const defaultData = {
+    id: '',
     email: '',
     firstName: '',
     lastName: '',
@@ -10,12 +11,62 @@ function UpdateUser() {
   };
 
   const [loaded, setLoaded] = useState(false);
-  const [FormData, setFormData] = useState(defaultData);
+  const [formData, setFormData] = useState(defaultData);
+  const handleChange = (event) => {
+      setFormData(data => ({
+          ...data,
+        [event.target.name]: event.target.value
+      }))
+  };
+
+  const handleSubmit = () => {
+
+  };
 
   return (
     <div className='UpdateUser'>
-      <form>
-           
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='id'>id</label>
+        <input
+          type='text'
+          onChange={handleChange}
+          name='id'
+          placeholder='id'
+          value={formData.id}
+        />
+        <label htmlFor='firstName'>First Name</label>
+        <input
+          type='text'
+          onChange={handleChange}
+          name='firstName'
+          placeholder='First Name'
+          value={formData.firstName}
+        />
+        {console.log({ formData })}
+
+        <label htmlFor='lastName'>Last Name</label>
+        <input
+          type='text'
+          onChange={handleChange}
+          name='lastName'
+          placeholder='Last Name'
+          value={formData.lastName}
+        />
+
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          onChange={handleChange}
+          name='email'
+          placeholder='Email'
+          value={formData.email}
+        />
+        <select name='state' onChange={handleChange} value={formData.state}>
+          <option value='pending'>Pending</option>
+          <option value='active'>Active</option>
+        </select>
+
+        <button>Submit</button>
       </form>
     </div>
   );
